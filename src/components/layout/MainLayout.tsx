bddf452c0c39065973356a712c0f7876a027ed0d
munir <smsirajulmonir@gmail.com>
2025-02-22 12:45:10 +0600

    layout set from antd

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,54 @@
+import { Breadcrumb, Layout, Menu } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+const items = Array.from({ length: 3 }).map((_, index) => ({
+  key: String(index + 1),
+  label: `nav ${index + 1}`,
+}));
+const MainLayout = () => {
+    
+    return (
+        <Layout>
+      <Header
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </Header>
+      <Content style={{ padding: '0 48px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          style={{
+            padding: 24,
+            minHeight: 380,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
+    );
+};
+
+export default MainLayout;
